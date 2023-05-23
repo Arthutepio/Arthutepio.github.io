013b9af558ed4c56da2ae737a9cd8f4f8fab9321
Arthutepio <arthurodriguesr10@gmail.com>
2023-05-23 02:05:12 -0300

    feat: cria tags de navegação do NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <>
+            <a href="">Sobre mim</a>
+            <a href="">Projetos</a>
+            <a href="">Conhecimentos</a>
+            <a href="">Contatos</a>
+        </>
+    );
+}
+
+export default NavBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+
+import NavBar from "../components/NavBar";
+
 export default function Home() {
     return (
         <>
-        <h1>Home</h1>
+        <NavBar />
         </>
     );
 
